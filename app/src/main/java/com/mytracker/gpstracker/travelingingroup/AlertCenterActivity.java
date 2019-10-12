2548f59c6066731260741d208ab5ffe01083cc10
eldadfertouk <eldad.fertouk@gmail.com>
2019-10-12 21:45:38 +0300

    12102019-full home

---
+++ app/src/main/java/com/mytracker/gpstracker/travelingingroup/AlertCenterActivity.java
@@ -0,0 +1,115 @@
+package com.mytracker.gpstracker.travelingingroup;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class AlertCenterActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    RecyclerView.Adapter recyclerAdapter;
+    RecyclerView.LayoutManager layoutManager;
+    ArrayList<CreateUser> myList;
+    FirebaseAuth auth;
+    FirebaseUser user;
+    String memberUserId;
+
+    CreateUser createUser;
+    DatabaseReference reference, userReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_alert_center );
+        recyclerView = findViewById ( R.id.alertRecyclerView );
+        layoutManager = new LinearLayoutManager ( this );
+        auth = FirebaseAuth.getInstance ();
+        user = auth.getCurrentUser ();
+        toolbar = findViewById ( R.id.toolbar );
+        toolbar.setTitle ( "מסך שירותי חירום והצלה" );
+        setSupportActionBar ( toolbar );
+
+
+        reference = FirebaseDatabase.getInstance ().getReference ().child ( "Users" ).child ( user.getUid () ).child ( "HelpAlerts" );
+        userReference = FirebaseDatabase.getInstance ().getReference ().child ( "Users" );
+        myList = new ArrayList<> ();
+
+
+        if (getSupportActionBar () != null) {
+            getSupportActionBar ().setDisplayHomeAsUpEnabled ( true );
+            getSupportActionBar ().setDisplayShowHomeEnabled ( true );
+        }
+
+        recyclerView.setLayoutManager ( layoutManager );
+        recyclerView.setHasFixedSize ( true );
+
+
+        reference.addValueEventListener ( new ValueEventListener () {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                myList.clear ();
+
+                if (dataSnapshot.exists ()) {
+                    for (DataSnapshot dss : dataSnapshot.getChildren ()) {
+                        memberUserId = Objects.requireNonNull ( Objects.requireNonNull ( dss.child ( "circlememberid" ).getValue () ) ).toString ();
+
+                        userReference.child ( memberUserId ).addListenerForSingleValueEvent ( new ValueEventListener () {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                createUser = dataSnapshot.getValue ( CreateUser.class );
+                                myList.add ( createUser );
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                                Toast.makeText ( getApplicationContext (), databaseError.getCode (), Toast.LENGTH_SHORT ).show ();
+                            }
+                        } );
+                    }
+                    Toast.makeText ( getApplicationContext (), "מציג התראות חירום", Toast.LENGTH_SHORT ).show ();
+                    recyclerAdapter = new HelpAlertsAdapter ( myList, getApplicationContext () );
+
+                    recyclerView.setAdapter ( recyclerAdapter );
+                    recyclerAdapter.notifyDataSetChanged ();
+                } else {
+                    Toast.makeText ( getApplicationContext (), "רשימת התראות חירום ריקה", Toast.LENGTH_SHORT ).show ();
+                    recyclerView.setAdapter ( null );
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText ( getApplicationContext (), databaseError.getMessage (), Toast.LENGTH_SHORT ).show ();
+            }
+        } );
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId () == android.R.id.home)
+            finish ();
+        return super.onOptionsItemSelected ( item );
+    }
+}
---
+++ app/src/main/java/com/mytracker/gpstracker/travelingingroup/SendHelpAlertsActivity.java
@@ -0,0 +1,125 @@
+package com.mytracker.gpstracker.travelingingroup;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import static java.lang.Thread.sleep;
+
+public class SendHelpAlertsActivity extends AppCompatActivity {
+
+    TextView t1_CounterTxt;
+    int countValue = 5;
+    Thread myThread;
+    DatabaseReference circlereference, usersReference, joinedCirclesReference;
+    FirebaseAuth auth;
+    FirebaseUser user;
+    String memberUserId;
+    ArrayList<String> userIDsList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_send_help_alerts );
+        t1_CounterTxt = findViewById ( R.id.textView9 );
+        auth = FirebaseAuth.getInstance ();
+        userIDsList = new ArrayList<> ();
+        user = auth.getCurrentUser ();
+        circlereference = FirebaseDatabase.getInstance ().getReference ().child ( "Users" ).child ( user.getUid () ).child ( "CircleMembers" );
+        joinedCirclesReference = FirebaseDatabase.getInstance ().getReference ().child ( "Users" ).child ( user.getUid () ).child ( "JoinedCircles" );
+        usersReference = FirebaseDatabase.getInstance ().getReference ().child ( "Users" );
+        myThread = new Thread ( new ServerThread () );
+        myThread.start ();
+    }
+
+    public void setCancel(View v) {
+
+        Toast.makeText ( getApplicationContext (), "ההתראה בוטלה !!!", Toast.LENGTH_SHORT ).show ();
+        myThread.interrupt ();
+        Intent myIntent = new Intent ( SendHelpAlertsActivity.this, MyTour.class );
+        startActivity ( myIntent );
+        finish ();
+    }
+
+    private class ServerThread implements Runnable {
+        @Override
+        public void run() {
+            try {//do some heavy task here on main separate thread like: Saving files in directory, any server operation or any heavy task
+                ///Once this task done and if you want to update UI the you can update UI operation on runOnUiThread method like this:
+                while (countValue != 0) {
+                    sleep ( 1000 );
+                    runOnUiThread ( new Runnable () {
+                        @Override
+                        public void run() {
+                            t1_CounterTxt.setText ( String.valueOf ( countValue ) );
+                            countValue = countValue - 1;
+                        }
+                    } );
+
+                }
+
+                runOnUiThread ( new Runnable () {
+                    @Override
+                    public void run() {
+                        circlereference.addValueEventListener ( new ValueEventListener () {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                userIDsList.clear ();
+                                for (DataSnapshot dss : dataSnapshot.getChildren ()) {
+                                    memberUserId = dss.child ( "circlememberid" ).getValue ( String.class );
+                                    userIDsList.add ( memberUserId );
+                                }
+                                if (userIDsList.isEmpty ()) {
+                                    Toast.makeText ( getApplicationContext (), "אין מטיילים בקבוצה , הוסף מטיילים", Toast.LENGTH_SHORT ).show ();
+                                } else {
+                                    CircleJoin circleJoin = new CircleJoin ( user.getUid () );
+                                    for (int i = 0; i < userIDsList.size (); i++) {
+                                        usersReference.child ( userIDsList.get ( i ) ).child ( "HelpAlerts" ).child ( user.getUid () ).setValue ( circleJoin )
+                                                .addOnCompleteListener ( new OnCompleteListener<Void> () {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if (task.isSuccessful ()) {
+                                                            Toast.makeText ( getApplicationContext (), "התראת חירום ואיתור נשלחה בהצלחה", Toast.LENGTH_SHORT ).show ();
+                                                        } else {
+                                                            Toast.makeText ( getApplicationContext (), "לא ניתן לשלוח התראה,בדוק חיבורים ונסה שוב", Toast.LENGTH_SHORT ).show ();
+                                                        }
+                                                    }
+                                                } );
+                                    }
+                                }
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                                Toast.makeText ( getApplicationContext (), databaseError.getMessage (), Toast.LENGTH_SHORT ).show ();
+                            }
+                        } );
+
+                    }
+                } );
+
+            } catch (Exception e) {
+                Toast.makeText ( getApplicationContext (), "משהו השתבש בתהליך השליחה,לא נשלחו התראות,נסה ערוץ חירום אחר", Toast.LENGTH_SHORT ).show ();
+            }
+        }
+    }
+
+}
